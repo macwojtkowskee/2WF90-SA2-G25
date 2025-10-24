6b098d7ec1feaa97d3564d6e6ccee3f9bc631fc6
UberGum123 <lsa1239087@gmail.com>
2025-10-24 21:57:15 +0200

    feat: solver

--- solve.py
+++ solve.py
@@ -9,14 +9,15 @@
 #
 # Author names and student IDs:
 # Maciej Wojtkowski (1984209)
-# author_name_2 (author_student_ID_2)
+# Sofiia Larina (1971018))
 # author_name_3 (author_student_ID_3)
 # author_name_4 (author_student_ID_4)
 ##
 
 # Import built-in json library for handling input/output 
 import json
-
+import poly_arithmetic as pa
+import finite_field_arithmetic as ffa
 
 def solve_exercise(exercise_location : str, answer_location : str):
     """
@@ -33,23 +34,106 @@ def solve_exercise(exercise_location : str, answer_location : str):
 
     ### Parse and solve ###
 
+    answer = {}
+    
+    p = exercise["integer_modulus"]
+    task = exercise["task"]
+    ex_type = exercise["type"]
+    
     # Check type of exercise
     if exercise["type"] == "polynomial_arithmetic":
-        # Check what task within the polynomial arithmetic tasks we need to perform
-        if exercise["task"] == "addition":
-            # Solve polynomial arithmetic addition exercise
-            pass
-        elif exercise["task"] == "subtraction":
-            # Solve polynomial arithmetic subtraction exercise
-            pass
-        # et cetera
-    else: # exercise["type"] == "finite_field_arithmetic"
-        # Check what task within the finite field arithmetic tasks we need to perform
-        if exercise["task"] == "addition":
-            # Solve finite field arithmetic addition exercise
-            pass
-        # et cetera
+        if task in ["addition", "subtraction", "multiplication", "long_division"                "extended_euclidean_algorithm"]:
+            f = pa.Polynomial(exercise["f"], p)
+            g = pa.Polynomial(exercise["g"], p)
+            # Check what task within the polynomial arithmetic tasks we need to perform
+            if exercise["task"] == "addition":
+                # Solve polynomial arithmetic addition exercise
+                result = f + g
+                answer["answer"] = result.coefficients
+                pass
+            elif exercise["task"] == "subtraction":
+                # Solve polynomial arithmetic subtraction exercise
+                result = f - g
+                answer["answer"] = result.coefficients
+                pass
+            elif task == "multiplication":
+                result = f * g
+                answer["answer"] = result.coefficients
+            
+            elif task == "long_division":
+                q, r = pa.polynomial_long_division(f, g)
+                if q is None:
+                    answer["answer-q"] = None
+                    answer["answer-r"] = None
+                else:
+                    answer["answer-q"] = q.coefficients
+                    answer["answer-r"] = r.coefficients
+                
+            elif task == "extended_euclidean_algorithm":
+                a, b, d = pa.extended_euclidean_algorithm(f, g)
+                answer["answer-a"] = a.coefficients
+                answer["answer-b"] = b.coefficients
+                answer["answer-gcd"] = d.coefficients
+                
+        elif task == "irreducibility_check":
+                f = pa.Polynomial(exercise["f"], p)
+                answer["answer"] = pa.is_irreducible(f, p)
+            
+        elif task == "irreducible_element_generation":
+            n = exercise["degree"]
+            poly = pa.generate_irreducible(p, n)
+            answer["answer"] = poly.coefficients
 
+    else: # exercise["type"] == "finite_field_arithmetic"
+        
+        h = pa.Polynomial(exercise["polynomial_modulus"], p)
+        
+        if task in ["addition", "subtraction", "multiplication", "division"]:
+            f = pa.Polynomial(exercise["f"], p)
+            g = pa.Polynomial(exercise["g"], p)
+            # Check what task within the finite field arithmetic tasks we need to perform
+            if exercise["task"] == "addition":
+                g = pa.Polynomial(exercise["g"], p)
+                result = f + g
+                result = pa.poly_mod_reduction(result, h)
+                answer["answer"] = result.coefficients
+                
+            elif task == "subtraction":
+                g = pa.Polynomial(exercise["g"], p)
+                result = f - g
+                result = pa.poly_mod_reduction(result, h)
+                answer["answer"] = result.coefficients
+                
+            elif task == "multiplication":
+                g = pa.Polynomial(exercise["g"], p)
+                result = f * g
+                result = pa.poly_mod_reduction(result, h)
+                answer["answer"] = result.coefficients
+                
+            elif task == "division":
+                g = pa.Polynomial(exercise["g"], p)
+                result = ffa.finite_field_division(f, g, h)
+                if result is None:
+                    answer["answer"] = None
+                else:
+                    answer["answer"] = result.coefficients
+        elif task == "inversion":
+            f = pa.Polynomial(exercise["f"], p)
+            f_inv = ffa.finite_field_inversion(f,h) #
+            if f_inv is None:
+                answer["answer"] = None
+            else:
+                answer["answer"] = f_inv.coefficients
+        elif task == "primitive_element_check":
+            f = pa.Polynomial(exercise["f"], p)
+            is_prim = ffa.is_primitive(f, h, p)
+            answer["answer"] = is_prim
+        elif task == "primitive_element_generation":
+             n = h.degree()
+             prim_elem = ffa.primitive_generation(h, p)
+             answer["answer"] = prim_elem.coefficients
+        # Solve finite field arithmetic addition exercise
+        # et cetera
 
     # Open file at answer_location for writing, creating the file if it does not exist yet
     # (and overwriting it if it does already exist).
